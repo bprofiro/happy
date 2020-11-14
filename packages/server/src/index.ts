f586734b744c3edc8e5bc9721a48203704d80941
Brenda Profiro <brendaprofiro@gmail.com>
2020-11-14 12:05:23 -0300

    feat: create and enable database

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+
+import './database/connection';
 import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 app.use(routes);
 
